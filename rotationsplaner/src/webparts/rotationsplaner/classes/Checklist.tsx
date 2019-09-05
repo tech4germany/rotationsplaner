e426ff12db65605ad275303548d80f186eed314f
Carl Gödecken <carl.julius.g@me.com>
2019-09-05 17:11:26 +0200

    Add initial data types

---
+++ rotationsplaner/src/webparts/rotationsplaner/classes/Checklist.tsx
@@ -0,0 +1,32 @@
+class LinkedItem {
+  public readonly description: string;
+  public readonly uri: string;
+}
+
+class Contact {
+}
+
+class TaskDescription {
+  public readonly id: string;
+  public readonly name: string;
+  public readonly detailText?: string;
+  public readonly links?: LinkedItem[];
+  public readonly pointOfContact?: Contact;
+  public readonly showOnlyFor?: Preference;
+  public readonly isCustom: Boolean;
+}
+
+class Task {
+  public readonly description: TaskDescription;
+  public checked: boolean;
+  public dueBy?: Date;
+}
+
+class Category {
+  public readonly tasks: Task[];
+}
+
+class Preference {
+  name: string;
+  description: string;
+}
