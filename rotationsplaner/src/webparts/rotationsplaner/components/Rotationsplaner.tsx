91490ca9e312a3673f72439482dfb32ffd4d9a0c
Stephan Detje <stephan.detje@student.hpi.uni-potsdam.de>
2019-10-01 16:48:32 +0200

    add message bar

--- rotationsplaner/src/webparts/rotationsplaner/components/Rotationsplaner.tsx
+++ rotationsplaner/src/webparts/rotationsplaner/components/Rotationsplaner.tsx
@@ -6,24 +6,30 @@ import {default as PlanerHeader} from './PlanerHeader';
 import api from '../api/api';
 import {Category, Preference} from '../classes/Checklist';
 import InfoSection from "./InfoSection";
+import {MessageBar, MessageBarType} from 'office-ui-fabric-react/lib/MessageBar';
 
 export interface RotationsplanerState {
   categories: Category[];
   preferences: Preference[];
   infoData: any[];
+  message: any;
 }
 
 export default class Rotationsplaner extends React.Component < IRotationsplanerProps, RotationsplanerState > {
   public state: RotationsplanerState = {
     categories: undefined,
     preferences: undefined,
-    infoData: undefined
+    infoData: undefined,
+    message: {
+      // type: MessageBarType.severeWarning,
+      // text: 'This is a test warning...'
+    },
   };
 
   public componentDidMount(): void {
-    this.fetchCategories().catch(console.error); // don't wait
-    this.fetchPreferences().catch(console.error); // don't wait
-    this.fetchInfoData().catch(console.error); // don't wait
+    this.fetchCategories().catch(this.handleError.bind(this)); // don't wait
+    this.fetchPreferences().catch(this.handleError.bind(this)); // don't wait
+    this.fetchInfoData().catch(this.handleError.bind(this)); // don't wait
   }
 
   private async fetchCategories(): Promise<void> {
@@ -44,6 +50,7 @@ export default class Rotationsplaner extends React.Component < IRotationsplanerP
   public render(): React.ReactElement<IRotationsplanerProps> {
     return(
       <div className={styles.rotationsplaner}>
+        {this.renderMessageBar()}
         <h1>Willkommen {this.props.userName}</h1>
         <p>Wir helfen Ihnen dabei, alle relevanten Informationen, Formulare, und To-Dos zu finden. Außerdem unterstützen wir Sie dabei, Ihre individuelle Checkliste anzulegen.</p>
         <p>Zunächst füllen Sie Ihre persönliche Angaben aus.</p>
@@ -62,8 +69,28 @@ export default class Rotationsplaner extends React.Component < IRotationsplanerP
     );
   }
 
+  private renderMessageBar() : React.ReactElement<{}> {
+    return (
+      <MessageBar className={this.state.message.text ? styles.contentVisible : styles.contentHidden}
+                  messageBarType={this.state.message.type}
+                  onDismiss={() => this.setState(prevState => ({...prevState, message: {}}))}>
+        {this.state.message.text}
+      </MessageBar>)
+  }
+
   private onPreferencesChanged(preferences: Preference[]): Promise<void> {
     this.setState(prevState => ({...prevState, preferences: preferences}));
     return api.postPreferences(preferences);
+  }
+
+  private handleError(error) {
+    console.error(error);
+    this.setState(prevState => ({
+      ...prevState,
+      message: {
+        type: MessageBarType.error,
+        text: error
+      }
+    }));
   }
 }
