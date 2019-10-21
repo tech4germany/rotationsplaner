1a13eea055a77bdeedd89deacf8e39067a633ebd
Carl Gödecken <carl.julius.g@me.com>
2019-10-21 08:36:38 +0200

    Release changes

--- rotationsplaner/deployment/import_site.ps1
+++ rotationsplaner/deployment/import_site.ps1
@@ -1,13 +1,13 @@
 #Set-PnPTraceLog -On -Level Debug
 
 # Url zur Sitecollection anpassen
-#$targetSiteUrl = "https://teams.bln-qsp.aa.bund.de/test/rotationsportal/"
-$targetSiteUrl = "https://rotationsportal.sp4.ovh.net/reimport/"
+$targetSiteUrl = "https://teams.bln-qsp.aa.bund.de/test/rotationsportal/"
+#$targetSiteUrl = "https://rotationsportal.sp4.ovh.net/reimport/"
 
-#Connect-PnPOnline -Url $targetSiteUrl -UseWebLogin
-Get-PnPContext
+Connect-PnPOnline -Url $targetSiteUrl #-UseWebLogin
+#Get-PnPContext
 # hier müssen ggf. noch einige Knoten aus der XML entfernt werden
-Apply-PnPProvisioningTemplate -Path rotationsportal_export.xml -ProvisionFieldsToSubWebs
+Apply-PnPProvisioningTemplate -Path rotationsportal_export.xml #-ProvisionFieldsToSubWebs
 
 # Inhalt der Site Assets (Skripte aus ../temp/deploy/)
 #Apply-PnPProvisioningTemplate -Path Provision_Site_Assets.xml
